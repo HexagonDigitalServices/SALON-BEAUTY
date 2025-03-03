de23483fd40be51b6bdd80b29fe6f5366ec94282
Hexagon Digital Services <hexagonsservices@gmail.com>
2025-03-03 18:46:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        Lets Build this Static Site
+        Let's Build this Static Site
       </h1>
     </div>
   )
