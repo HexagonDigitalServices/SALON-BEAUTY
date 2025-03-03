2c2c53c57ec14751a828d4185c6ff33ef2b840ad
SrivastavaRachit <rachitsrivastava@gmail.com>
2025-03-03 18:44:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Lets Build this Static Site
+      </h1>
+    </div>
+  )
+}
+
+export default App
